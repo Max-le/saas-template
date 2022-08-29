e90ba3cd97ca108dde3a5a5e5b4d83510788bbc2
max <max.09@outlook.com>
2022-08-29 07:42:39 +0200

    first

--- front-end/deployment/config/config-development.ts
+++ front-end/deployment/config/config-development.ts
@@ -1,7 +1,7 @@
 /**
  * Add your hosted dev URL here
  */
-const HOSTED_URL = 'https://dev.domain.com';
+const HOSTED_URL = 'https://main.d1h66vrvs37kht.amplifyapp.com/';
 
 const configDevelopment = {
   HOSTED_URL,
@@ -14,10 +14,10 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '',
-  USER_POOL_ID: '',
-  IDENTITY_POOL_ID: '',
-  GRAPHQL_ENDPOINT: '',
+  USER_POOL_CLIENT_ID: '5spn0a4ot6l5ea3tl2dp2jqkib',
+  USER_POOL_ID: 'eu-west-1_g1vN3bMjA',
+  IDENTITY_POOL_ID: 'eu-west-1:a763b632-8534-4eb8-970f-b81d75e3bd54',
+  GRAPHQL_ENDPOINT: '"https://j5tbi5lxzvgq5ggo6qaya5d5gm.appsync-api.eu-west-1.amazonaws.com/graphql',
 };
 
 export default configDevelopment;
